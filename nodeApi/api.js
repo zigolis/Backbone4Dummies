3a5dca46e6f853c6e36a84dd59dc1122f291786e
Renan Carvalho <renan.carvalho@getchute.com>
2015-06-11 20:09:44 -0300

    Now it uses node

---
+++ nodeApi/api.js
@@ -0,0 +1,16 @@
+function setup(app) {
+  app.post('/*/article',function () {
+  	
+  });
+  app.put('/article', function () {
+		
+	});
+	app.get('/*/article/:id', function () {
+		
+	});
+	app.get('/*/articles',function(){
+		
+	});
+}
+ 
+exports.setup = setup;
